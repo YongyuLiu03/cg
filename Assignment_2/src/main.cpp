f348648f70ddf9f6e50940d38bf56755b4ee6f95
Yongyu Liu <yl8126@nyu.edu>
2023-02-15 00:30:45 -0500

    done perspective

--- Assignment_2/src/main.cpp
+++ Assignment_2/src/main.cpp
@@ -82,26 +82,37 @@ void raytrace_parallelogram() {
 	Vector3d y_displacement(0,-2.0/C.rows(),0);
 
 	// TODO: Parameters of the parallelogram (position of the lower-left corner + two sides)
-	Vector3d pgram_origin;
-	Vector3d pgram_u;
-	Vector3d pgram_v;
-
+	Vector3d pgram_origin(-0.8, -0.8, 0);
+	Vector3d pgram_u(1.2, 0.4, 0);
+	Vector3d pgram_v(0.4, 1.2, 0);
+ 
 	// Single light source
-	const Vector3d light_position(-1,1,1);
+	const Vector3d light_position(0,0,2);
 
 	for (unsigned i=0; i < C.cols(); ++i) {
 		for (unsigned j=0; j < C.rows(); ++j) {
 			// Prepare the ray
 			Vector3d ray_origin = origin + double(i)*x_displacement + double(j)*y_displacement;
 			Vector3d ray_direction = RowVector3d(0,0,-1);
-
 			// TODO: Check if the ray intersects with the parallelogram
-			if (true) {
+			Vector3d RHS(ray_origin-pgram_origin);
+			Matrix3d LHS;
+			LHS << pgram_u, pgram_v, -ray_direction;
+			LHS.transposeInPlace();
+			Vector3d x = LHS.colPivHouseholderQr().solve(RHS);
+			double u, v, t;
+			u = x(0);
+			v = x(1);
+			t = x(2);
+
+			if (u>=0 && u<=1 && v>=0 && v<=1 && t>0) {
 				// TODO: The ray hit the parallelogram, compute the exact intersection point
-				Vector3d ray_intersection(0,0,0);
+				Vector3d ray_intersection(ray_origin + t*ray_direction);
 
 				// TODO: Compute normal at the intersection point
-				Vector3d ray_normal = ray_intersection.normalized();
+				Vector3d intersect_u = pgram_u;
+				Vector3d intersect_v = pgram_v;
+				Vector3d ray_normal = (intersect_u.cross(intersect_v)).normalized();
 
 				// Simple diffuse model
 				C(i,j) = (light_position-ray_intersection).normalized().transpose() * ray_normal;
@@ -114,15 +125,15 @@ void raytrace_parallelogram() {
 			}
 		}
 	}
-
 	// Save to png
 	write_matrix_to_png(C,C,C,A,filename);
+	
 }
 
 void raytrace_perspective() {
 	std::cout << "Simple ray tracer, one parallelogram with perspective projection" << std::endl;
 
-	const std::string filename("plane_perspective.png");
+	const std::string filename1("parallelogram_perspective.png");
 	MatrixXd C = MatrixXd::Zero(800,800); // Store the color
 	MatrixXd A = MatrixXd::Zero(800,800); // Store the alpha mask
 
@@ -132,9 +143,9 @@ void raytrace_perspective() {
 	Vector3d y_displacement(0,-2.0/C.rows(),0);
 
 	// TODO: Parameters of the parallelogram (position of the lower-left corner + two sides)
-	Vector3d pgram_origin;
-	Vector3d pgram_u;
-	Vector3d pgram_v;
+	Vector3d pgram_origin(-0.8, -0.8, 0);
+	Vector3d pgram_u(1.2, 0.4, 0);
+	Vector3d pgram_v(0.4, 1.2, 0);
 
 	// Single light source
 	const Vector3d light_position(-1,1,1);
@@ -142,16 +153,26 @@ void raytrace_perspective() {
 	for (unsigned i=0; i < C.cols(); ++i) {
 		for (unsigned j=0; j < C.rows(); ++j) {
 			// TODO: Prepare the ray (origin point and direction)
-			Vector3d ray_origin = origin + double(i)*x_displacement + double(j)*y_displacement;
-			Vector3d ray_direction = RowVector3d(0,0,-1);
-
+			Vector3d ray_origin = origin;
+			Vector3d ray_direction = Vector3d(origin(0), origin(1), 0) + double(i)*x_displacement + double(j)*y_displacement - ray_origin;
+			
 			// TODO: Check if the ray intersects with the parallelogram
-			if (true) {
+			Vector3d RHS(ray_origin-pgram_origin);
+			Matrix3d LHS;
+			LHS << pgram_u, pgram_v, -ray_direction;
+			Vector3d x = LHS.colPivHouseholderQr().solve(RHS);
+			double u, v, t;
+			u = x(0);
+			v = x(1);
+			t = x(2);
+			if (u>=0 && u<=1 && v>=0 && v<=1 && t>0) {
 				// TODO: The ray hit the parallelogram, compute the exact intersection point
-				Vector3d ray_intersection(0,0,0);
+				Vector3d ray_intersection(ray_origin + t*ray_direction);
 
 				// TODO: Compute normal at the intersection point
-				Vector3d ray_normal = ray_intersection.normalized();
+				Vector3d intersect_u = pgram_u;
+				Vector3d intersect_v = pgram_v;
+				Vector3d ray_normal = (intersect_u.cross(intersect_v)).normalized();
 
 				// Simple diffuse model
 				C(i,j) = (light_position-ray_intersection).normalized().transpose() * ray_normal;
@@ -166,7 +187,49 @@ void raytrace_perspective() {
 	}
 
 	// Save to png
-	write_matrix_to_png(C,C,C,A,filename);
+	write_matrix_to_png(C,C,C,A,filename1);
+
+	std::cout << "Simple ray tracer, one sphere with perspective projection" << std::endl;
+
+	const std::string filename2("sphere_perspective.png");
+	C = MatrixXd::Zero(800,800); // Store the color
+	A = MatrixXd::Zero(800,800); // Store the alpha mask
+
+	Vector3d sphere_origin(0, 0, 0);
+	const double sphere_radius = 0.3;
+
+	for (unsigned i=0; i < C.cols(); ++i) {
+		for (unsigned j=0; j < C.rows(); ++j) {
+			// TODO: Prepare the ray (origin point and direction)
+			Vector3d ray_origin = origin;
+			Vector3d ray_direction = Vector3d(origin(0), origin(1), 0) + double(i)*x_displacement + double(j)*y_displacement - ray_origin;
+
+			// Intersect with the sphere
+			const double a = ray_direction.squaredNorm();
+			const double b = 2*ray_direction.dot(ray_origin-sphere_origin);
+			const double c = (ray_origin-sphere_origin).squaredNorm() - pow(sphere_radius, 2);
+			const double d = pow(b, 2) - 4*a*c;
+
+			if (d >= 0) {
+				// The ray hit the sphere, compute the exact intersection point
+				const double t = (-b-sqrt(d))/(2*a);
+				Vector3d ray_intersection = ray_origin + t*ray_direction;
+				// Compute normal at the intersection point
+				Vector3d ray_normal = ray_intersection.normalized();
+
+				// Simple diffuse model
+				C(i,j) = (light_position-ray_intersection).normalized().transpose() * ray_normal;
+
+				// Clamp to zero
+				C(i,j) = std::max(C(i,j),0.);
+
+				// Disable the alpha mask for this pixel
+				A(i,j) = 1;
+			}
+		}
+	}
+
+	write_matrix_to_png(C,C,C,A,filename2);
 }
 
 void raytrace_shading(){
@@ -226,10 +289,10 @@ void raytrace_shading(){
 }
 
 int main() {
-	raytrace_sphere();
-	raytrace_parallelogram();
+	// raytrace_sphere();
+	// raytrace_parallelogram();
 	raytrace_perspective();
-	raytrace_shading();
+	// raytrace_shading();
 
 	return 0;
 }
